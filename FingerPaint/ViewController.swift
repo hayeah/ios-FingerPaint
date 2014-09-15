67090b399b17b24f0171a2f627bce5c149e9e31d
Howard Yeh <howard@metacircus.com>
2014-09-15 09:57:44 +0800

    Initial Commit

---
+++ FingerPaint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FingerPaint
+//
+//  Created by Howard Yeh on 2014-09-15.
+//  Copyright (c) 2014 Howard Yeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
